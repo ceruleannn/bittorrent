97c6b547090fb09e51c93699cc83c03c3149aaaf
piao <1712439537@qq.com>
2019-03-13 11:58:57 +0800

    init

---
+++ bt-crawler/src/main/java/com/piao/bittorrent/crawler/BtCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.piao.bittorrent.crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtCrawlerApplication.class, args);
+    }
+
+}
---
+++ bt-web/src/test/java/com/piao/bittorrent/web/BtWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.piao.bittorrent.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BtWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
